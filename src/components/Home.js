9ed3d726b44ebcd49193da837407261de20789a8
Guillermo Alfaro <galfarotolon@gmail.com>
2020-08-11 16:25:25 -0500

    added Banner over hero images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Hero from '../components/Hero'
-
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <div>
 
-            <Hero />
+            <Hero >
+
+                <Banner title='luxurious rooms'
+                    subtitle='deluxe rooms starting at $299'>
+                    <Link to='/rooms' className='btn-primary'>
+                        Our Rooms
+                    </Link>
+
+
+                </Banner>
+            </Hero>
 
         </div>
     )
