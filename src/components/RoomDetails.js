dce2087e1549f04020923f26d4239fd18472659a
Guillermo Alfaro <galfarotolon@gmail.com>
2020-11-11 18:53:50 -0500

    styled components for dynamic images

--- src/components/RoomDetails.js
+++ src/components/RoomDetails.js
@@ -9,22 +9,39 @@ export default class RoomDetails extends Component {
 
     constructor(props) {
         super(props);
-        console.log(this.props);
+        //console.log(this.props);
         this.state = {
             slug: this.props.match.params.slug,
             defaultBcg: defaultBcg
         };
     }
+
+    static contextType = RoomContext;
+
     //compontent 
     componentDidMount() {
 
     }
 
     render() {
-        return (
-            <div>
-                Room Details Page here
+        const { getRoom } = this.context;
+        const room = getRoom(this.state.slug)
+        //change this to if(loading) when grabbing from API
+        if (!room) {
+            return <div class="error"><h3>This room couldn't be found</h3>
+                <Link to='/rooms' className='btn-primary'>Back To Rooms</Link>
             </div>
-        )
+        }
+
+        const { name, description, capacity, size, price, extras, breakfast,
+            pets, images } = room
+        return <Hero hero='roomsHero'>
+            <Banner title={`${name} room`}>
+                <Link to='/rooms' className='btn-primary'>
+                    Back to Rooms
+                </Link>
+            </Banner>
+
+        </Hero>
     }
 }
---
+++ src/components/StyledHero.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import React from 'react'
+
+const SimpleButton = styled.button`
+color: red;
+background: green;
+font-size: 3rem;
+
+
+`
+
+
+
+
+
+export default SimpleButton
+
