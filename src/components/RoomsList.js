f9efc9263b95d24fa1afc8a6bd08589bdd6af1ea
Guillermo Alfaro <galfarotolon@gmail.com>
2020-11-12 13:45:39 -0500

    room list completed

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import Room from './Room'
 
-function RoomsList() {
+function RoomsList({ rooms }) {
+
+    if (rooms.length === 0) {
+        return (
+            <div className='empty-search'>
+                <h3>No Rooms Matched Your Search Parameters</h3>
+            </div>
+        )
+    }
+
+
     return (
-        <div>
-            Roomslist
-        </div>
+        <section className='roomslist'>
+            <div className='roomslist-center'>
+                {
+                    rooms.map(room => {
+                        return <Room key={room.id} room={room} />
+
+                    })
+                }
+            </div>
+        </section>
     )
 }
 
