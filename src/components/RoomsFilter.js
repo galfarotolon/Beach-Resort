712f0881eb9d1d590f721b2cbc5c26d749ef17be
Guillermo Alfaro <galfarotolon@gmail.com>
2020-11-12 19:35:01 -0500

    finished filters for room list

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -43,7 +43,40 @@ function RoomsFilter({ rooms }) {
                     <select name='capacity' id='capacity' value={capacity}
                         className='form-control' onChange={handleChange}>{guests}</select>
                 </div>
+                <div className='form-group'>
+                    <label htmlFor='price'>{`Price: $${price} `}</label>
+                    <input type='range' name='price'
+                        min={minPrice} max={maxPrice}
+                        id='price' value={price}
+                        onChange={handleChange} className='form-control' />
 
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='size'>Room Size</label>
+                    <div className='size-inputs'>
+                        <input type="number" name='minSize' id='size'
+                            value={minSize} onChange={handleChange} className='size-input' />
+                        <input type="number" name='maxSize' id='size'
+                            value={maxSize} onChange={handleChange} className='size-input' />
+
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <div className='single-extra'>
+                        <input type='checkbox' name='breakfast'
+                            id='breakfast' checked={breakfast} onChange={handleChange} />
+
+                        <label htmlFor="breakfast">Breakfast</label>
+                    </div>
+                    <div className='single-extra'>
+                        <input type='checkbox' name='pets'
+                            id='pets' checked={pets} onChange={handleChange} />
+
+                        <label htmlFor="pets">Pets</label>
+                    </div>
+
+                </div>
 
             </form>
         </section>
--- src/context.js
+++ src/context.js
@@ -65,7 +65,7 @@ class RoomProvider extends Component {
 
     handleChange = (e) => {
         const target = e.target
-        const value = e.type === 'checkbox' ? target.checked : target.value
+        const value = target.type === 'checkbox' ? target.checked : target.value
         const name = e.target.name
 
         this.setState({
@@ -80,8 +80,9 @@ class RoomProvider extends Component {
         // all the rooms
         let tempRooms = [...rooms];
 
-        //make capacity into int
+        //transform value
         capacity = parseInt(capacity)
+        price = parseInt(price)
 
         //filter by room type
         if (type !== 'all') {
@@ -93,6 +94,20 @@ class RoomProvider extends Component {
             tempRooms = tempRooms.filter(room => room.capacity >= capacity)
         }
 
+        //filter by price
+        tempRooms = tempRooms.filter(room => room.price <= price)
+
+        //filter by room size
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+
+
+        //filter by extras
+        if (breakfast) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+        if (pets) {
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
 
         this.setState({
             sortedRooms: tempRooms
